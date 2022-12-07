c5220d28e1dd549f68da02d20437caff0b3b9c5b
Philip Maechling <pjmaechling@users.noreply.github.com>
2022-12-07 11:57:06 -0800

    updated ucvm plot scripts

---
+++ convert_cross_section.sh
@@ -0,0 +1,1 @@
+./ucvm_cross_section2csv.py 2ddata/cross-cvmsi_data.bin 2ddata/cross-cvmsi_meta.json
---
+++ convert_horizontal_slices.sh
@@ -0,0 +1,1 @@
+./ucvm_horizontal_slice2csv.py 2ddata/cvms_poisson_map_data.bin 2ddata/cvms_poisson_map_meta.json
